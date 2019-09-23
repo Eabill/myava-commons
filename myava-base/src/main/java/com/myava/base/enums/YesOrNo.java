e8895374a6126916256d2451c8010c0f5e8f7641
lin_yi_biao <linyibiao@zuzuche.com>
2019-09-23 23:31:36 +0800

    base模块集成log

---
+++ myava-base/src/main/java/com/myava/base/enums/YesOrNo.java
@@ -0,0 +1,29 @@
+package com.myava.base.enums;
+
+/**
+ * 是否枚举
+ *
+ * @author biao
+ */
+public enum YesOrNo {
+
+    NO(0, "否"),
+    YES(1, "是");
+
+    private Integer code;
+
+    private String description;
+
+    YesOrNo(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- myava-mybatis/src/main/java/com/myava/mybatis/mapper/EmailNotifyErrLogMapper.java
+++
@@ -1,8 +0,0 @@
-package com.myava.mybatis.mapper;
-
-import com.myava.mybatis.entity.EmailNotifyErrLog;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailNotifyErrLogMapper extends BaseMapper<EmailNotifyErrLog> {
-}
