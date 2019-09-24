dcade11c2a13e6d71e2534a6de07099c0cc864fa
linyibiao <linyibiao2010@qq.com>
2019-09-24 19:57:40 +0800

    tk-mybatis动态表名

---
+++ myava-mybatis/src/main/java/com/myava/mybatis/AbstractDynamicTable.java
@@ -0,0 +1,35 @@
+package com.myava.mybatis;
+
+import com.myava.mybatis.annotation.DynamicTable;
+import lombok.Getter;
+import lombok.Setter;
+import tk.mybatis.mapper.entity.IDynamicTableName;
+
+import javax.persistence.Transient;
+import java.lang.reflect.Field;
+
+/**
+ * 动态表名抽象类
+ *
+ * @author biao
+ */
+public abstract class AbstractDynamicTable implements IDynamicTableName {
+
+    @Setter
+    @Getter
+    @Transient
+    protected String tableName;
+
+    @Override
+    public String getDynamicTableName() {
+        try {
+            DynamicTable dynamicTable = this.getClass().getAnnotation(DynamicTable.class);
+            Field field = this.getClass().getDeclaredField(dynamicTable.field());
+            field.setAccessible(true);
+            int tabInx = Math.abs(field.get(this).hashCode()) % dynamicTable.capacity();
+            return dynamicTable.prefix() + dynamicTable.symbol() + tabInx;
+        } catch (Exception ex) {
+            throw new RuntimeException("DynamicTableName get error.", ex);
+        }
+    }
+}
---
+++ myava-mybatis/src/main/java/com/myava/mybatis/annotation/DynamicTable.java
@@ -0,0 +1,42 @@
+package com.myava.mybatis.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 动态表注解
+ *
+ * @author biao
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+public @interface DynamicTable {
+
+    /**
+     * 分表前缀
+     * @return
+     */
+    String prefix();
+
+    /**
+     * 分表容量
+     * @return
+     */
+    int capacity() default 0;
+
+    /**
+     * 分表字段
+     * @return
+     */
+    String field() default "id";
+
+    /**
+     * 连接符
+     * @return
+     */
+    String symbol() default "-";
+}
