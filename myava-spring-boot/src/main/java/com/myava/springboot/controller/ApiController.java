96651f5f6284d0c7a6a0d60fd93d3471bcde7199
lin_yi_biao <linyibiao@zuzuche.com>
2020-06-13 17:12:50 +0800

    引入swagger文档、统一异常处理

--- myava-spring-boot/src/main/java/com/myava/springboot/controller/ApiController.java
+++ myava-spring-boot/src/main/java/com/myava/springboot/controller/ApiController.java
@@ -10,7 +10,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Min;
@@ -34,7 +33,7 @@ public class ApiController {
     }
 
     @GetMapping("/get")
-    public RespResult<UserEntity> get(@RequestParam @Min(value = 1, message = "id:必须正整数") Integer id) {
+    public RespResult<UserEntity> get(@Min(value = 1, message = "id:必须正整数") Integer id) {
         return RespResult.success(userService.get(id));
     }
 
